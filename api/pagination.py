d5df4ee936d76a997419708ac9d752cf5cf0ce2f
MawmartA7 <aaronstmart.dev@gmail.com>
2025-09-18 16:26:06 -0300

    feat(pagination): add size query parameter on pagination

--- api/pagination.py
+++ api/pagination.py
@@ -5,6 +5,10 @@ from rest_framework.response import Response
 class CustomPagination(PageNumberPagination):
     page_size = 10
 
+    page_size_query_param = 'size'
+
+    max_page_size = 50
+
     def get_paginated_response(self, data):
         return Response({
             "count": self.page.paginator.count,
