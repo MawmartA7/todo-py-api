ad5d89fb91233c8029da30e84cec8f5f2e3a2359
MawmartA7 <aaronstmart.dev@gmail.com>
2025-09-25 15:53:36 -0300

    feat(api): add drf-spectacular to use OpenAPI

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('api/auth/', include("authentication.urls")),
-    path('api/tasks/', include("todo.urls"))
+    path('api/tasks/', include("todo.urls")),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+
 ]
